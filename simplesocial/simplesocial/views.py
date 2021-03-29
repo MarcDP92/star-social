aa91503af01ca06b17aa2d942efc8136b7a5ead8
Kitus <marcandorra92@gmail.com>
2021-03-29 16:34:02 +0200

    uploading project

---
+++ simplesocial/simplesocial/views.py
@@ -0,0 +1,12 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+
+class LoggedPage(TemplateView):
+    template_name = 'logged.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
